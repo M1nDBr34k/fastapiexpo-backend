e5591aee1b30bee67b32af896eee9a5ba9d9b7d7
M1nDBr34k <151804171+M1nDBr34k@users.noreply.github.com>
2025-04-24 23:54:48 +0800

    Clarify comment for CORS middleware to specify that all HTTP methods are allowed

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["*"],    # Allows all HTTP methods   
     allow_headers=["*"],
 )
 
